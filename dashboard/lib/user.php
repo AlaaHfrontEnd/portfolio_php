81326f4cbd5ddaf0f266cd22b2d29b2ddc123e1f
Alaa-H-frontEnd <alaah0153@gmail.com>
2020-09-15 18:36:32 +0200

    First

---
+++ dashboard/lib/user.php
@@ -0,0 +1,32 @@
+<?php
+
+function addNewUser($name, $email, $password){
+
+    $con = mysqli_connect("localhost", "root", "", "fs8_proj1");
+
+    $sql = "INSERT INTO `user` (`name`, `email`, `password`) VALUES ('$name', '$email', '$password')";
+    mysqli_query($con, $sql);
+
+    $res = mysqli_affected_rows($con);
+    if($res==1){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function login($email, $password){
+    $con = mysqli_connect("localhost", "root", "", "fs8_proj1");
+    
+    $sql = "SELECT * FROM `user` WHERE `email`='$email' && `password`='$password'";
+   
+    $q = mysqli_query($con, $sql);
+
+   $res = mysqli_fetch_assoc($q);
+
+   return $res;
+
+
+  
+  
+}
